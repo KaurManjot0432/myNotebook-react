945dcba28718c94ae5dac7eb88e4a6d5506d36c5
KaurManjot0432 <0432manjot@gmail.com>
2021-10-02 20:41:09 +0530

    basic router setup and schemas

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
-const connect = require('./db');
 const express = require('express')
+const connect = require('./src/config/db');
+const router = require('./src/routes/index');
 
 connect();  
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+app.use('/',router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
